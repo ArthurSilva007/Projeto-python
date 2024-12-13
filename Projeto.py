fd5386f4ffede290e511542665dae8a9ea5dd55a
Arthur Silva007 <andersonarthur740@gmail.com>
2024-12-13 12:29:21 -0300

    update

--- Projeto.py
+++ Projeto.py
@@ -32,3 +32,31 @@ pdf.text(20, 175, texto3)
 pdf.text(20, 185, texto4)
 
 pdf.output(f"certificado_sauer.pdf")
+
+# from fpdf import FPDF
+
+# title = "CERTIFICADO DE PARTICIPAÇÃO"
+# subtitulo = "Este certificado comprova que"
+# aluno = "Anderson Silva"
+# texto2 = "Concluiu com êxito o curso GRATUITO DE PYTHON ministrado por"
+# texto3 = "PROF. SAUER entre 13/12/2022 e 16/12/2022."
+# texto4 = "com carga horária de aproximadamente 08 horas"
+
+# pdf = FPDF()
+
+# pdf.add_page()
+
+# pdf.set_font("Arial", "B", size=15)
+
+# pdf.image("template.png", x=0, y=0, w=210, h=297)
+
+# pdf.set_text_color(33, 24, 136)
+
+# pdf.text(65, 95, title)
+# pdf.text(67, 120, subtitulo)
+# pdf.text(70, 145, aluno)
+# pdf.text(20, 165, texto2)
+# pdf.text(20, 175, texto3)
+# pdf.text(20, 185, texto4)
+
+# pdf.output("certificado_sauer.pdf")
